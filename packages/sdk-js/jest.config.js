4b74905f7b0cd116f50784ac44a75ac24496400b
jithinraj <jjitinraj@gmail.com>
2025-07-21 16:13:14 +0530

    feat(http402): Add HTTP 402 Payment Required handler and tests (Node.js/Python)

--- packages/sdk-js/jest.config.js
+++ packages/sdk-js/jest.config.js
@@ -1,5 +1,9 @@
 // jest.config.js
 module.exports = {
   testEnvironment: 'node',
-  testMatch: ['**/core/tests/**/*.test.js'],
+  testMatch: [
+    '**/core/tests/**/*.test.js',
+    '**/core/ed25519/node/__tests__/**/*.test.js',
+    '**/core/interop/http402/node/__tests__/**/*.test.js'
+  ],
 };
