3f6a8aba654e787484b64e06125001aa3c912801
jithinraj <jjitinraj@gmail.com>
2025-07-21 16:52:18 +0530

    fix(lite-mode): Fix jest config, add Node.js tests for Lite Mode

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
   testMatch: [
     '**/core/tests/**/*.test.js',
     '**/core/ed25519/node/__tests__/**/*.test.js',
-    '**/core/interop/http402/node/__tests__/**/*.test.js'
-  ],
+    '**/core/nonce/__tests__/**/*.test.js',
+    '**/core/interop/http402/node/__tests__/**/*.test.js',
+    '**/core/interop/lite_mode/node/__tests__/**/*.test.js'
+  ]
 };
