3ad7135ee3632eda0d5a32ef6345922bd7a221aa
Jithin Raj <jjitinraj@gmail.com>
2025-07-22 04:48:33 +0530

    Delete legacy_core directory
    chore: fully remove legacy_core after v0.9.1 merge cleanup

--- legacy_core/hash.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-function getTermsHash(terms) {
-  const termsString = JSON.stringify(terms, Object.keys(terms).sort());
-  return crypto.createHash('sha256').update(termsString).digest('hex');
-}
-
-module.exports = { getTermsHash };
